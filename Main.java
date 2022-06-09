0dd0eeaff0ff7c8d333c4b723f1ec2de1b93625f
rodasalfaro <rodasalfaro@gmail.com>
2022-06-08 21:08:06 -0600

    Version 1 tarea3

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        sumafunc(10,20,30);
+
+    }
+
+    public static void sumafunc(int val1, int val2, int val3){
+        int sumaresultado;
+        sumaresultado = val1+val2+val3;
+
+        System.out.println(sumaresultado);
+    }
+}
+
+    
