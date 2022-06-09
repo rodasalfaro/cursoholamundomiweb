7535ed4fa4d79ac7bf7c447e20c798950a829edc
rodasalfaro <rodasalfaro@gmail.com>
2022-06-08 21:30:26 -0600

    Version 1 tarea3

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 public class Main {
 
     public static void main(String[] args) {
+        Coche miCarro = new Coche();
 
+        miCarro.SumarPuerta();
+        miCarro.SumarPuerta();
+        System.out.println(miCarro.puerta);
+
+
+        //Seccion de llamada de funcion suma de 3 variables por medio de paso por valor
         sumafunc(10,20,30);
 
     }
 
+    //Seccion del codigo de la funcion de suma de tres variables
     public static void sumafunc(int val1, int val2, int val3){
         int sumaresultado;
         sumaresultado = val1+val2+val3;
@@ -13,5 +21,12 @@ public class Main {
         System.out.println(sumaresultado);
     }
 }
+//Clase Coche
+class Coche{
+    public int puerta = 0;
 
+    public void SumarPuerta(){
+        this.puerta++;
+    }
+}
     
